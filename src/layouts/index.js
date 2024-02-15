29a864b0d28ad939db77e2b177c3a847821f4b81
CollinBarlow <collinbarlow@gmail.com>
2024-02-15 11:41:42 -0800

    added components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import Card from '../components/Card'
 
 const Layout = ({ children, data }) => (
   <div>
