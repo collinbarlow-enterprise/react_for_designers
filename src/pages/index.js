42e7ad3baac95091101c77602f24c248bc9d31da
CollinBarlow <collinbarlow@gmail.com>
2024-02-15 10:30:01 -0800

    animation added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to design and code React Apps</h1>
+        <h1>Learn to  <br/>design and code React Apps</h1>
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
         <Link to="/video">Watch the video</Link>
         <Link to="/page-2/">Go to page 2</Link>
